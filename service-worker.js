cc56c73fc446a89d8865553039ebbe3730b4df62
balazsabalogh <baloghbalazsattila@gmail.com>
2026-08-21 19:15:10 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,27 @@
-const CACHE = 'tc-antalyabeta-v060';
-const ASSETS = ['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+const CACHE = 'tc-antalyabeta-v061';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './logo-tr.png',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './ANTALYA_CONTENT.json'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
 self.addEventListener('fetch', e => {
   if(e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-    const copy = resp.clone();
-    caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
-    return resp;
-  }).catch(() => caches.match('./index.html'))));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
 });
